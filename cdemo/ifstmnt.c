9e9238670eaf1f826d35f86d79d4d63b379fb0f4
Sofia Kwee <sofiakwee@gmail.com>
2019-05-22 17:22:49 +0000

    not done

---
+++ cdemo/ifstmnt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+  int b= 10;
+  // if statement to test is a is equal to 0
+  if (a == 0)
+  {
+    printf("a is 0\n");
+  }
+  else if (a == b)
+  {
+     printf("a is equal to b")
+  else
+  {
+    printf("a is not 0\n");
+  }
+    
+}
